df6fa2337345995826dd38f06394ac8a3c7a4b34
Nikita Strygin <nikita6@bk.ru>
2022-10-21 20:56:11 +0300

    Add a dummy bevy project

--- shin/src/main.rs
+++ shin/src/main.rs
@@ -1,5 +1,22 @@
 use anyhow::Result;
+use bevy::prelude::*;
 
-fn main() -> Result<()> {
-    Ok(())
+fn hello_world() {
+    trace!("!!! hello world !!!");
+    println!("hello world!");
+}
+
+fn main() {
+    App::new()
+        .insert_resource(bevy::render::settings::WgpuSettings {
+            // we don't actually need a beefy GPU for this
+            power_preference: bevy::render::settings::PowerPreference::LowPower,
+            ..Default::default()
+        })
+        .add_plugins_with(DefaultPlugins, |group| {
+            // here we can modify the default plugins
+            group
+        })
+        .add_startup_system(hello_world)
+        .run();
 }
