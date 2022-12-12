388b5c6f548db8da5ab8ce20cba614286d05e511
Nikita Strygin <nikita6@bk.ru>
2022-12-12 19:31:50 +0300

    Add docs for lz77 module

--- shin-core/src/format/lz77.rs
+++ shin-core/src/format/lz77.rs
@@ -1,3 +1,19 @@
+//! This module contains implementation of the variant of lz77 used in the game
+//!
+//! To distinguish between literals and references by using bitmaps.
+//!
+//! These bitmaps are 8-bit integers, where each bit represents either one literal byte or one reference.
+//!
+//! The references are encoded as 16-bit big-endian (sic!) integers. It encodes offset and length of the reference,
+//!     but the amount of bits spent on each part is dependent on the format (I use const generics for this).
+//!
+//! The minimum offset is 1, so the actual offset is offset + 1.
+//! The minimum length is 3, so the actual length is length + 3.
+//!
+//! Encoding is (to be) implemented using a sliding window and a greedy algorithm.
+//! Theoretically the efficiency can be improved by using a bit of backtracking,
+//!     but it seems this improves compression ratio only by several percent (not worth the time).
+
 use bytes::Buf;
 use std::io;
 
@@ -30,6 +46,8 @@ pub fn decompress<const OFFSET_BITS: u32>(input: &[u8], output: &mut Vec<u8>) {
 
                 for _ in 0..len {
                     let last = output.len() - back_offset as usize;
+                    // TODO: make this fallible?
+                    // TODO: this might be optimized by stopping the bounds checking after we have enough data to guarantee that it's in bounds
                     output.push(output[last]);
                 }
             }
