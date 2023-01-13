3d10236a66c73a6d99eeb399356c0a75f3ed46a3
Nikita Strygin <nikita6@bk.ru>
2023-01-13 17:30:58 +0300

    Add some basic subcommands cli to the junk executable

--- junk/src/main.rs
+++ junk/src/main.rs
@@ -1,7 +1,23 @@
-//
+use clap::{Args, Parser, Subcommand};
+use std::path::PathBuf;
 
 mod buffer_parser;
 
+// use clap to select what to do
+#[derive(Parser)]
+struct Opts {
+    #[clap(subcommand)]
+    action: JunkAction,
+}
+
+#[derive(Parser)]
+enum JunkAction {
+    BufferParser,
+}
+
 fn main() {
-    buffer_parser::main();
+    let opts = Opts::parse();
+    match opts.action {
+        JunkAction::BufferParser => buffer_parser::main(),
+    }
 }
