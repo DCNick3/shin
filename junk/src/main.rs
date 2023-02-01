27304d4ad5da48821f4acd2b48f830239f3c9c18
Nikita Strygin <nikita6@bk.ru>
2023-02-01 18:38:17 +0300

    Extract debug texture from the executable

--- junk/src/main.rs
+++ junk/src/main.rs
@@ -2,6 +2,7 @@ use clap::{Args, Parser, Subcommand};
 use std::path::PathBuf;
 
 mod buffer_parser;
+mod debug_tex_parser;
 
 // use clap to select what to do
 #[derive(Parser)]
@@ -13,11 +14,13 @@ struct Opts {
 #[derive(Parser)]
 enum JunkAction {
     BufferParser,
+    DebugTexParser,
 }
 
 fn main() {
     let opts = Opts::parse();
     match opts.action {
         JunkAction::BufferParser => buffer_parser::main(),
+        JunkAction::DebugTexParser => debug_tex_parser::main(),
     }
 }
