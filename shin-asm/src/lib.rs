69fb74928c7f2591b4894a640a66acc8e6be4291
Nikita Strygin <nikita6@bk.ru>
2023-10-22 02:54:45 +0300

    Add doc for shin-asm crate

--- shin-asm/src/lib.rs
+++ shin-asm/src/lib.rs
@@ -1,3 +1,11 @@
+//! An assembler for shin scenarios.
+//!
+//! The parser's design is heavily inspired by rust-analyzer's parser.
+//!
+//! The later stages of compilation diverge more, but are also somewhat inspired by rust-analyzer.
+//!
+//! Its design (hopefully) will allow for easier integration with an IDE later down the line.
+
 // macro hack
 extern crate self as shin_asm;
 
